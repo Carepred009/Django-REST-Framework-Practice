668f17326d67569dd6df6645fc68771f39c1de7c
Carepred <carepred.ballos88@gmail.com>
2025-11-19 19:45:14 +0800

    How to use Components in Vue

--- frontend/myprojectvue/src/main.js
+++ frontend/myprojectvue/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+//from FoodItem.vue
+import FoodItem from './components/FoodItem.vue'
+
 import router from './router'
+
+
 const app = createApp(App)
+
+//food-item will be use in the App.vue
+app.component('food-item', FoodItem)
 
 app.use(router)
 
