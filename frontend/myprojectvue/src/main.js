3197195052c2c08972985bcd87b8644fbd382823
Carepred <carepred.ballos88@gmail.com>
2025-11-23 20:35:06 +0800

    Create form with v-model

--- frontend/myprojectvue/src/main.js
+++ frontend/myprojectvue/src/main.js
@@ -1,16 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-//from FoodItem.vue
-//import FoodItem from './components/FoodItem.vue'
 
-//import AddItem from './components/AddItem.vue'
-
-//for PATCH-PUT DRF-Vue-axios
-//import UpdateBook from './components/UpdateBook.vue'
-
-//for delete PATCH-PUT DRF-Vue-axios
-//import DeleteItem from './components/DeleteItem.vue'
 
 //v-model sample
 import Foods from './components/Foods.vue'
@@ -18,29 +9,25 @@ import Foods from './components/Foods.vue'
 //input-field sample
 import InputFields from './components/InputFields.vue'
 
+
+import VForms_sample from './components/VForms_sample.vue' //import the VForms_sample.vue and pass to the object
+
 import router from './router'
 
 
 const app = createApp(App)
 
+//pass the data to this
+app.component('v-form_sample',  VForms_sample )
 
 //input fields
 app.component('input-fields', InputFields )
 
-//food-item will be use in the App.vue
-//app.component('food-item', FoodItem)
-//app.component('add-item', AddItem)
 
-//v-model sample
 app.component('food-name', Foods)
 
 
 
-//for //for delete PATCH-PUT DRF-Vue-axios
-//app.component('delete-item', DeleteItem)
-
-//for PATCH-PUT DRF-Vue-axios
-//app.component('update-book',UpdateBook)
 
 app.use(router)
 
