fe93692090af0fe6a48321eeebdef730736d66e9
Carepred <carepred.ballos88@gmail.com>
2025-11-23 17:11:09 +0800

    v-model example and Vue Props example

---
+++ frontend/myprojectvue/src/components/InputFields.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Two Inputs + Show on Button Click</h2>
+
+    <label>First Name:</label>
+    <input v-model="firstName" type="text" />
+
+    <label>Last Name:</label>
+    <input v-model="lastName" type="text" />
+
+    <button @click="showText">Show Output</button>
+
+    <h3 v-if="output">Result:</h3>
+    <p>{{ output }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      output: ""
+    };
+  },
+
+  methods: {
+    showText() {
+      this.output = `Hello ${this.firstName} ${this.lastName}!`;
+    }
+  }
+};
+</script>
